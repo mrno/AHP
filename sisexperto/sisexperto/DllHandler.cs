a86c78dea0bceb45878c4435292d70e2babc534f
pc-del-gisia <frre.gisia@gmail.com>
2012-09-04 21:02:06 -0300

    Inclui la conexion a las dlls

---
+++ sisexperto/sisexperto/DllHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MathWorks.MATLAB.NET.Arrays;
+using MathWorks.MATLAB.NET.Utility;
+
+
+namespace sisexperto
+{
+    public class DllHandler
+    {
+
+        public Boolean calcularConsistencia(double[,] matriz)
+        {
+
+          
+            return true;
+        }
+
+
+
+
+    }
+}
