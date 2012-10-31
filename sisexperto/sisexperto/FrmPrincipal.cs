c7a2cbdecf5e1d805c651591694b008b0ec1664b
pc-del-gisia <frre.gisia@gmail.com>
2012-10-31 18:47:50 -0300

    se agredo la agregacion ponderada2

--- sisexperto/sisexperto/FrmPrincipal.cs
+++ sisexperto/sisexperto/FrmPrincipal.cs
@@ -4,8 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+using MathWorks.MATLAB.NET.Arrays;
+using CalcularAHP;
+using Consistencia;
+using Mejora;
 
 namespace sisexperto
 {
@@ -51,6 +56,22 @@ namespace sisexperto
         */
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
+
+
+            var foo1 = typeof(CalculoAHP);
+            var foo2 = typeof(MWArray);
+            var foo3 = typeof(Consistencia.Consistencia);
+            var foo4 = typeof(Mejora.Consistencia);
+           
+
+            //Assembly CalcularAHP = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\CalcularAHP.dll");
+            //Assembly CalcularAHPNative = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\CalcularAHPNative.dll");
+            //Assembly Consistencia = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\Consistencia.dll");
+            //Assembly ConsistenciaNative = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\ConsistenciaNative.dll");
+            //Assembly Mejora = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\Mejora.dll");
+            //Assembly MejoraNative = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\MejoraNative.dll");
+            //Assembly MWArray = Assembly.LoadFile(@"C:\Documents and Settings\gisiaa\Mis documentos\GitHub\AHP\Dll\MWArray.dll");
+
 
         }
 
