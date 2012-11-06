2b9f8ac81046fea8d5dba762379ca7dbe0dc34ff
pc-del-gisia <frre.gisia@gmail.com>
2012-11-06 20:22:29 -0300

    se avanza en la migracion, Calculo AHP NO Ponderado4

--- sisexperto/sisexperto/Fachadas/FachadaEjecucionProyecto.cs
+++ sisexperto/sisexperto/Fachadas/FachadaEjecucionProyecto.cs
@@ -2,39 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using sisexperto.Entidades;
+using sisExperto.Entidades;
 
-namespace sisexperto.Fachadas
+namespace sisExperto.Fachadas
 {
     public class FachadaEjecucionProyecto
     {
-        private DALDatos datos = new DALDatos();
         private GisiaExpertoContext _context = new GisiaExpertoContext();
+        private FachadaProyectosExpertos _fachadaProyectosExpertos = new FachadaProyectosExpertos();
+        public Experto Proyecto { get; set; }
 
-        public experto Proyecto { get; set; }
-
-
-        //ASI SERIA EL NUEVO
-        //Esto accede a la nueva DB
         public IEnumerable<ExpertoEnProyecto> ObtenerExpertosProyecto(Proyecto _proyecto)
         {
             return _proyecto.ExpertosAsignados;
         }
 
-        //Esto accede a la vieja DB
-        public List<experto_proyecto> ObtenerExpertosProyecto(proyecto _proyecto)
-        {
-            return datos.expertosPorProyecto2(_proyecto.id_proyecto);
-        }
+    
 
-        public void GuardarCambios(List<experto_proyecto> _expertosConPonderacion)
+
+        public void GuardarCambios(List<ExpertoEnProyecto> _ExpertosConPonderacion)
         {
-            throw new NotImplementedException();
+            foreach (var Experto in _ExpertosConPonderacion)
+            {
+                
+            }
         }
 
         public bool PosibleEjecutarAHP()
         {
             return true;
         }
+
+
+        public void EjecucionAHPNoPonderado(Proyecto _proyecto) {
+
+
+            var listaExperto = ExpertoProyectoConsistentes(_proyecto);
+            List<double[,]> listaKMatrizCriterios = new List<double[,]>();
+            List<double[,]> listaKNMatrizAlternativas = new List<double[,]>();
+            List<double[,]> listaCompleta = new List<double[,]>();
+            if (listaExperto.ToList().Count() != 0)
+            {
+                var AgregacionNOPonderada = new AgregacionNoPonderada();
+                
+
+                foreach (ExpertoEnProyecto exp in listaExperto)
+                {
+                    listaKMatrizCriterios.Add(exp.ValoracionCriteriosPorExperto.Matriz);
+                    //foreach (ValoracionAlternativasPorCriterioExperto valAlt in exp.ValoracionAlternativasPorCriterioExperto) {
+                    //    listaKNMatrizAlternativas.Add(valAlt.Matriz);
+                    //}         
+
+                    listaCompleta.Add(AgregacionNOPonderada.AgregarCriterios(listaKMatrizCriterios));
+
+
+                }
+
+                CalculoAHP calculo = new CalculoAHP();
+                ranking = calculo.calcularRanking(listaCompleta);
+
+
+
+
+
+                CalcularAhpAgregado frmAhpAgregado = new CalcularAhpAgregado(ranking, _proyectoSeleccionado.id_proyecto);
+                frmAhpAgregado.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Ningún experto ha valorado de manera consistente.");
+            }
+
+        
+        
+        
+        
+        }
+
+        public IEnumerable<ExpertoEnProyecto> ExpertoProyectoConsistentes(Proyecto _proyecto)
+        {
+            return _fachadaProyectosExpertos.ObtenerExpertosProyectoConsistente(_proyecto);
+                              }
     }
 }
