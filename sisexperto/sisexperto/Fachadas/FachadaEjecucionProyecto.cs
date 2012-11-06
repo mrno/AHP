1c3bfcf1411a3a6e9f52c3e48038d05d5aa18f5d
pc-del-gisia <frre.gisia@gmail.com>
2012-11-06 18:04:06 -0300

    agregue la metodo de EjecucionAHPNP a la fachada, no anda aun

--- sisexperto/sisexperto/Fachadas/FachadaEjecucionProyecto.cs
+++ sisexperto/sisexperto/Fachadas/FachadaEjecucionProyecto.cs
@@ -36,5 +36,61 @@ namespace sisExperto.Fachadas
         {
             return true;
         }
+
+
+        public void EjecucionAHPNoPonderado(Proyecto _proyecto) {
+
+
+            listaExperto = dato.expeProyConsistente(_proyectoSeleccionado.id_proyecto);
+
+            if (listaExperto.Count != 0)
+            {
+                foreach (experto exp in listaExperto)
+                {
+
+                    AgrAlternativas altAgregar = new AgrAlternativas(_proyectoSeleccionado.id_proyecto, exp.id_experto);
+                    listaAlternativasPonderar.Add(altAgregar);
+                }
+
+                matrizCriterioPonderar = new AgrCriterio(_proyectoSeleccionado.id_proyecto);
+
+                //Acá procedo a agregarle la primer matriz, la de criterios:
+
+                listaCompleta.Add(calculadorNoPonderadas.AgregarCriterios(matrizCriterioPonderar));
+
+                //Acá creo una lista con las alternativas ponderadas en la primer línea y luego la recorro y para cada elemento le asigno
+                //su valor de atributo a la listaCompleta:
+
+                listaNAlt = calculadorNoPonderadas.AgregarAlternativas(listaAlternativasPonderar);
+
+                foreach (NAlternativas alt in listaNAlt)
+                {
+                    listaCompleta.Add(alt.nAlternativas);
+                }
+
+                //Luego de todo este despelote, listaCompleta está terminada para pasarse a la clase CalculoAHP.
+
+                CalculoAHP calculo = new CalculoAHP();
+                ranking = calculo.calcularRanking(listaCompleta);
+
+
+
+
+
+                CalcularAhpAgregado frmAhpAgregado = new CalcularAhpAgregado(ranking, _proyectoSeleccionado.id_proyecto);
+                frmAhpAgregado.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Ningún experto ha valorado de manera consistente.");
+            }
+
+        
+        
+        
+        
+        }
+
+
     }
 }
