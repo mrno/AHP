72d5ca86b031012f38e4ec9f9abb864dc39bf767
pc-del-gisia <frre.gisia@gmail.com>
2012-11-23 21:02:44 -0300

    Muestra las etiquetas, cambio de branch porque no se donde estan las clases de AHP y eso

--- sisexperto/sisexperto/UI/ComparacionAlternativas.cs
+++ sisexperto/sisexperto/UI/ComparacionAlternativas.cs
@@ -97,6 +97,46 @@ namespace sisExperto
             return 0;
         }
 
+        public string valorarPalabra(int valor)
+        {
+            if (valor == 1)//corresponde a 9
+                return "[9] es extremadamente más importante que ";
+            if (valor == 2)//corresponde a 8
+                return "[8] ";
+            if (valor == 3)//corresponde a 7
+                return "[7] es muy fuertemente más importante que ";
+            if (valor == 4)//corresponde a 6
+                return "[6] ";
+            if (valor == 5)//corresponde a 5
+                return "[5] es fuertemente más importante que ";
+            if (valor == 6)//corresponde a 4
+                return "[4] ";
+            if (valor == 7)//corresponde a 3
+                return "[3] es moderadamente más importante que ";
+            if (valor == 8)//corresponde a 2
+                return "[2] ";
+            if (valor == 9)//corresponde a 1
+                return "[1] es igual de importante que ";
+            if (valor == 10)//corresponde a 1/2
+                return "[1/2] ";
+            if (valor == 11)//corresponde a 1/3
+                return "[1/3]es moderadamente menos importante que ";
+            if (valor == 12)//corresponde a 1/4
+                return "[1/4] ";
+            if (valor == 13)//corresponde a 1/5
+                return "[1/5] es fuertemente menos importante que ";
+            if (valor == 14)//corresponde a 1/6
+                return "[1/6] ";
+            if (valor == 15)//corresponde a 1/7
+                return "[1/7] es muy fuertemente menos importante que ";
+            if (valor == 16)//corresponde a 1/8
+                return "[1/8] ";
+            if (valor == 17)//corresponde a 1/9
+                return "[1/9] es extremadamente menos importante que ";
+
+            return "";
+        }
+
         private void mostrar(object sender, EventArgs e)
         {
             //label9.Text = "";
@@ -111,8 +151,8 @@ namespace sisExperto
                     if (miLabel.Name.ToString() == track.Name.ToString())
                     {
                         string[] posicion = track.Name.ToString().Split('x');
-                        //Label l = (Label)miLabel;
-                        //l.Text = dato.valorarPalabra(track.Value);
+                        Label l = (Label)miLabel;
+                        l.Text = valorarPalabra(track.Value);
                         
                         foreach (AlternativaFila fila in matrizAlternativa.FilasAlternativa)
                         {
