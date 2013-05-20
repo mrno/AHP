8440ae2b85a032d0f0474e83b719b825fdcba637
leonardo <leonardo.wanderer@gmail.com>
2013-05-20 19:30:38 -0300

    a

--- sisexperto/sisexperto/UI/CrearProyecto.Designer.cs
+++ sisexperto/sisexperto/UI/CrearProyecto.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBoxProyectos = new System.Windows.Forms.ComboBox();
+            this.proyectoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBoxClonacion = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.comboBoxTipoModelo = new System.Windows.Forms.ComboBox();
             this.labelNombreExperto = new System.Windows.Forms.Label();
@@ -41,10 +45,6 @@
             this.buttonCrearYContinuar = new System.Windows.Forms.Button();
             this.buttonLimpiar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
-            this.checkBoxClonacion = new System.Windows.Forms.CheckBox();
-            this.comboBoxProyectos = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.proyectoBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.proyectoBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -70,13 +70,52 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Datos de proyecto";
             // 
+            // comboBoxProyectos
+            // 
+            this.comboBoxProyectos.DataSource = this.proyectoBindingSource;
+            this.comboBoxProyectos.DisplayMember = "Nombre";
+            this.comboBoxProyectos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxProyectos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxProyectos.FormattingEnabled = true;
+            this.comboBoxProyectos.Location = new System.Drawing.Point(82, 55);
+            this.comboBoxProyectos.Name = "comboBoxProyectos";
+            this.comboBoxProyectos.Size = new System.Drawing.Size(372, 28);
+            this.comboBoxProyectos.TabIndex = 2;
+            this.comboBoxProyectos.ValueMember = "Nombre";
+            // 
+            // proyectoBindingSource
+            // 
+            this.proyectoBindingSource.DataSource = typeof(sisExperto.Entidades.Proyecto);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Proyecto:";
+            // 
+            // checkBoxClonacion
+            // 
+            this.checkBoxClonacion.AutoSize = true;
+            this.checkBoxClonacion.Location = new System.Drawing.Point(6, 25);
+            this.checkBoxClonacion.Name = "checkBoxClonacion";
+            this.checkBoxClonacion.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBoxClonacion.Size = new System.Drawing.Size(165, 24);
+            this.checkBoxClonacion.TabIndex = 0;
+            this.checkBoxClonacion.Text = "Crear por clonación";
+            this.checkBoxClonacion.UseVisualStyleBackColor = true;
+            this.checkBoxClonacion.CheckedChanged += new System.EventHandler(this.checkBoxClonacion_CheckedChanged);
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(6, 92);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(164, 20);
-            this.label4.TabIndex = 4;
+            this.label4.TabIndex = 3;
             this.label4.Text = "Modelo/s que ejecuta:";
             // 
             // comboBoxTipoModelo
@@ -90,7 +129,7 @@
             this.comboBoxTipoModelo.Location = new System.Drawing.Point(176, 89);
             this.comboBoxTipoModelo.Name = "comboBoxTipoModelo";
             this.comboBoxTipoModelo.Size = new System.Drawing.Size(278, 28);
-            this.comboBoxTipoModelo.TabIndex = 0;
+            this.comboBoxTipoModelo.TabIndex = 4;
             // 
             // labelNombreExperto
             // 
@@ -99,7 +138,7 @@
             this.labelNombreExperto.Location = new System.Drawing.Point(83, 230);
             this.labelNombreExperto.Name = "labelNombreExperto";
             this.labelNombreExperto.Size = new System.Drawing.Size(51, 20);
-            this.labelNombreExperto.TabIndex = 3;
+            this.labelNombreExperto.TabIndex = 10;
             this.labelNombreExperto.Text = "label4";
             // 
             // label1
@@ -108,7 +147,7 @@
             this.label1.Location = new System.Drawing.Point(6, 230);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 20);
-            this.label1.TabIndex = 2;
+            this.label1.TabIndex = 9;
             this.label1.Text = "Creador:";
             // 
             // label2
@@ -117,7 +156,7 @@
             this.label2.Location = new System.Drawing.Point(7, 126);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 20);
-            this.label2.TabIndex = 0;
+            this.label2.TabIndex = 5;
             this.label2.Text = "Nombre:";
             // 
             // label3
@@ -126,7 +165,7 @@
             this.label3.Location = new System.Drawing.Point(6, 158);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(70, 20);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 7;
             this.label3.Text = "Objetivo:";
             // 
             // textBoxObjetivoProyecto
@@ -135,14 +174,14 @@
             this.textBoxObjetivoProyecto.Multiline = true;
             this.textBoxObjetivoProyecto.Name = "textBoxObjetivoProyecto";
             this.textBoxObjetivoProyecto.Size = new System.Drawing.Size(372, 72);
-            this.textBoxObjetivoProyecto.TabIndex = 2;
+            this.textBoxObjetivoProyecto.TabIndex = 8;
             // 
             // textBoxNombreProyecto
             // 
             this.textBoxNombreProyecto.Location = new System.Drawing.Point(82, 123);
             this.textBoxNombreProyecto.Name = "textBoxNombreProyecto";
             this.textBoxNombreProyecto.Size = new System.Drawing.Size(372, 26);
-            this.textBoxNombreProyecto.TabIndex = 1;
+            this.textBoxNombreProyecto.TabIndex = 6;
             // 
             // buttonCrearYContinuar
             // 
@@ -176,45 +215,6 @@
             this.btnCancelar.Text = "Salir";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
-            // 
-            // checkBoxClonacion
-            // 
-            this.checkBoxClonacion.AutoSize = true;
-            this.checkBoxClonacion.Location = new System.Drawing.Point(6, 25);
-            this.checkBoxClonacion.Name = "checkBoxClonacion";
-            this.checkBoxClonacion.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.checkBoxClonacion.Size = new System.Drawing.Size(165, 24);
-            this.checkBoxClonacion.TabIndex = 5;
-            this.checkBoxClonacion.Text = "Crear por clonación";
-            this.checkBoxClonacion.UseVisualStyleBackColor = true;
-            this.checkBoxClonacion.CheckedChanged += new System.EventHandler(this.checkBoxClonacion_CheckedChanged);
-            // 
-            // comboBoxProyectos
-            // 
-            this.comboBoxProyectos.DataSource = this.proyectoBindingSource;
-            this.comboBoxProyectos.DisplayMember = "Nombre";
-            this.comboBoxProyectos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxProyectos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxProyectos.FormattingEnabled = true;
-            this.comboBoxProyectos.Location = new System.Drawing.Point(82, 55);
-            this.comboBoxProyectos.Name = "comboBoxProyectos";
-            this.comboBoxProyectos.Size = new System.Drawing.Size(372, 28);
-            this.comboBoxProyectos.TabIndex = 30;
-            this.comboBoxProyectos.ValueMember = "Nombre";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(6, 58);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 20);
-            this.label5.TabIndex = 31;
-            this.label5.Text = "Proyecto:";
-            // 
-            // proyectoBindingSource
-            // 
-            this.proyectoBindingSource.DataSource = typeof(sisExperto.Entidades.Proyecto);
             // 
             // CrearProyecto
             // 
